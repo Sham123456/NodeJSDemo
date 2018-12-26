8e54052ffbb5ca07dc6983c286c2b83e1e3ce4f9
Shamik Mukherjee <shamikmukherjee95@gmail.com>
2018-12-26 23:44:54 +0530

    Adding Storystrap template

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.set('views', './src/views');
+app.set('view template', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index.ejs');
 });
 
-app.listen(3000, () => {
-  debug(`Listening on port : ${chalk.green(3000)}`);
+app.listen(port, () => {
+  debug(`Listening on port : ${chalk.green(port)}`);
 });
