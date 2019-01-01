b6f67d7c98f15ccc4668f0e6ebb18e4c6713642f
Shamik Mukherjee <shamikmukherjee95@gmail.com>
2019-01-01 14:49:34 +0530

    Adding bookRoutes as a function

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const bookRouter = require('./src/routes/bookRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -15,6 +14,12 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.set('views', './src/views');
 app.set('view template', 'ejs');
 
+const nav = [
+    {title: 'Books', link: '/books'},
+    {title: 'Authors', link: '/authors'}
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use('/books', bookRouter);
 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,42 +1,40 @@
 const express = require('express');
 const booksRouter = express.Router();
 
-const books = [
-    {
-        name: 'Hamlet',
-        author: 'William Shakespeare'
-    },
-    {
-        name: 'Dracula',
-        author: 'Bram Stoker'
-    },
-    {
-        name: 'David Copperfield',
-        author: 'Charles Dickens'
-    }
-];
+function router(nav) {
+    const books = [
+        {
+            name: 'Hamlet',
+            author: 'William Shakespeare'
+        },
+        {
+            name: 'Dracula',
+            author: 'Bram Stoker'
+        },
+        {
+            name: 'David Copperfield',
+            author: 'Charles Dickens'
+        }
+    ];
 
-booksRouter.route('/').get((reg, res) => {
-    res.render('index.ejs', {
-        nav: [
-            {title: 'Books', link: '/books'},
-            {title: 'Authors', link: '/authors'}
-        ],
-        title: 'Library',
-        books
-    })
-});
+    booksRouter.route('/').get((reg, res) => {
+        res.render('index.ejs', {
+            nav,
+            title: 'Library',
+            books
+        })
+    });
 
-booksRouter.route('/:id').get((req, res) => {
-    const { id } = req.params;
-    res.render('book.ejs', {
-        nav: [
-            {title: 'Books', link: '/books'},
-            {title: 'Authors', link: '/authors'}
-        ],
-        title: 'Library',
-        book : books[id]
-    })
-});
+    booksRouter.route('/:id').get((req, res) => {
+        const { id } = req.params;
+        res.render('book.ejs', {
+            nav,
+            title: 'Library',
+            book : books[id]
+        })
+    });
 
-module.exports = booksRouter;
+    return booksRouter;
+}
+
+module.exports = router;
